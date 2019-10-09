05618de0c25f7fe7cf308de991b0fe3d976459b2
Zeljko <olah.zeljko@gmail.com>
2019-10-09 13:42:05 +0200

    Initial project setup and tests

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,24 +1,95 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
 
-import { Appointment } from '../src/Appointment';
+import { Appointment, AppointmentsDayView } from '../src/Appointment';
 
-it('renders the customer first name', () => {
-  const customer = { firstName: 'Ashley' };
-  const component = <Appointment customer={customer} />;
-  const container = document.createElement('div');
+describe('Appointment', () => {
+  let container;
+  let customer;
+  let component;
 
-  ReactDOM.render(component, container);
+  beforeEach(() => {
+    container = document.createElement('div');
+    component = <Appointment customer={customer} />;
+  });
 
-  expect(container.textContent).toMatch('Ashley');
+  const render = component => ReactDOM.render(component, container);
+
+  it('renders the customer first name', () => {
+    customer = { firstName: 'Ashley' };
+    render(<Appointment customer={customer} />);
+    expect(container.textContent).toMatch('Ashley');
+  });
+
+  it('renders another customer first name', () => {
+    customer = { firstName: 'Jordan' };
+    render(<Appointment customer={customer} />);
+    expect(container.textContent).toMatch('Jordan');
+  });
 });
 
-it('renders another customer first name', () => {
-  const customer = { firstName: 'Jordan' };
-  const component = <Appointment customer={customer} />;
-  const container = document.createElement('div');
+describe('AppointmentsDayView', () => {
+  let container;
+  const today = new Date();
+  const appointments = [
+    {
+      startsAt: today.setHours(12, 0),
+      customer: { firstName: 'Ashley' }
+    },
+    {
+      startsAt: today.setHours(13, 0),
+      customer: { firstName: 'Jordan' }
+    }
+  ];
 
-  ReactDOM.render(component, container);
+  beforeEach(() => {
+    container = document.createElement('div');
+  });
 
-  expect(container.textContent).toMatch('Jordan');
+  const render = component => ReactDOM.render(component, container);
+
+  it('renders a div with the right id', () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    expect(container.querySelector('div#appointmentsDayView')).not.toBeNull();
+  });
+
+  it('renders multiple appointments in an ol element', () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.querySelector('ol')).not.toBeNull();
+    expect(container.querySelector('ol').children).toHaveLength(2);
+  });
+
+  it('renders each appointment in an li', () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.querySelectorAll('li')).toHaveLength(2);
+    expect(container.querySelectorAll('li')[0].textContent).toEqual('12:00');
+    expect(container.querySelectorAll('li')[1].textContent).toEqual('13:00');
+  });
+
+  it('initially shows a message saying there are no appointments today', () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    expect(container.textContent).toMatch(
+      'There are no appointments scheduled for today.'
+    );
+  });
+
+  it('selects the first appointment by default', () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.textContent).toMatch('Ashley');
+  });
+
+  it('has a button element in each li', () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.querySelectorAll('li > button')).toHaveLength(2);
+    expect(container.querySelectorAll('li > button')[0].type).toEqual('button');
+  });
+
+  it('renders another appointment when selected', () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    const button = container.querySelectorAll('button')[1];
+    ReactTestUtils.Simulate.click(button);
+    expect(container.textContent).toMatch('Jordan');
+    console.log('container.textContent:', container.textContent);
+  });
 });
